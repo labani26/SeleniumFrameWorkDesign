e7be7f93451cd188f2d473e3d3eb1c766d95a2e3
Labani sardar <mail2labanisardar@gmail.com>
2026-08-12 06:31:45 -0500

    12/8/2026 2nd commit

--- src/test/java/RahulShettyAcademyPractice/SeleniumFrameWorkDesign/ErrorValidations.java
+++ src/test/java/RahulShettyAcademyPractice/SeleniumFrameWorkDesign/ErrorValidations.java
@@ -8,13 +8,17 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import PageObjects.CartPage;
+import PageObjects.CheckoutPage;
+import PageObjects.ConfirmationPage;
 import PageObjects.LandingPage;
+import PageObjects.ProductCatalogue;
 import RahulShettyacademy.TestComponents.BaseTest;
 
 public class ErrorValidations extends BaseTest {
 
     @Test
-    public void SubmitOrder() throws IOException, InterruptedException {
+    public void LoginErrorValidation() throws IOException, InterruptedException {
     	
     	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
@@ -30,6 +34,35 @@ public class ErrorValidations extends BaseTest {
         );
 
         Assert.assertEquals("Incorrect Email or Password", landingPage.getErrorMessage());
+    }
+    
+    @Test
+    public void ProductErrorValidation() throws IOException, InterruptedException {
+    	
+    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        String productName = "IPHONE 13 PRO";
+
+        // Launch application
+        LandingPage landingPage = launchApplication();
+
+        // Login
+        ProductCatalogue productCatalogue = landingPage.LoginApplication(
+        		"mail2labanisardar@gmail.com",
+                "Labani@26"
+        );
+
+        // Add product to cart
+        productCatalogue.AddProductToCart(productName);
+
+        // Go to Cart
+        CartPage cartPage = productCatalogue.CartPage();
+
+        // Verify product is displayed in cart
+        Boolean match = cartPage.VerifyProductDisplay("IPHONE 13 PRRO");
+        Assert.assertTrue(match);
+
+       
     }
 }
 
